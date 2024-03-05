fd9017e774243686374d62c97ad8164bbb04eb8b
snwmilap <milap@softnweb.in>
2024-03-05 18:43:23 +0530

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -39,33 +39,33 @@ const PostList: React.FC = () => {
     }
   };
 
-  if (loading) {
-    return (
-      <div className="">
-        Loading...
-      </div>
-    );
-  }
-
   return (
     <div className="xl:p-20 p-10">
-      <Link href={'/'} className="text-5xl font-bold my-10 inline-block">Back</Link>
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-none gap-10">
-        {data.map((post) => (
-          <div
-            key={post.id}
-            className="bg-white border p-4 shadow-md rounded-md"
-          >
-            <Link
-              href={`/${post.id}`}
-              className="font-semibold text-xl mb-3 block capitalize "
-            >
-              {post.title}
-            </Link>
-            <p>{post.body}</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Link href={"/"} className="text-5xl font-bold my-10 inline-block">
+            Back
+          </Link>
+          <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-none gap-10">
+            {data.map((post) => (
+              <div
+                key={post.id}
+                className="bg-white border p-4 shadow-md rounded-md"
+              >
+                <Link
+                  href={`/${post.id}`}
+                  className="font-semibold text-xl mb-3 block capitalize "
+                >
+                  {post.title}
+                </Link>
+                <p>{post.body}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
       {error && (
         <div className="text-red-800">
           <p>Error: {error}</p>
